0bd13963529c55ea4dc762b554c8d7ef01275b73
Ubuntu <avleensm@login.stud.ntnu.no>
2020-02-07 10:46:14 +0000

    lagt til mappe

---
+++ bookface/config.php
@@ -0,0 +1,11 @@
+<?php
+$dbhost = "192.168.130.12";
+$dbport =  "26257";
+$db = "bf";
+$dbuser = "bfuser";
+$dbpassw = '';
+$webhost = "10.212.140.71";
+$weburl = 'http://' . $webhost ;
+$frontpage_limit = "500";
+}
+?>
