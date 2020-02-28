775d97b70133bc03ed9cab90170cd6eaef4ab0dc
asm492 <60274300+asm492@users.noreply.github.com>
2020-02-28 16:04:52 +0100

    Update containerstart.sh

--- scripts/containerstart.sh
+++ scripts/containerstart.sh
@@ -3,8 +3,8 @@
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker container rm www11 www10 www9
 
-sudo docker run -d --restart always --name=www9 -p 8009:80 bf:v1
-sudo docker run -d --restart always --name=www10 -p 8010:80 bf:v1
-sudo docker run -d --restart always --name=www11 -p 8011:80 bf:v1
-/home/ubuntu/discord.sh-1.3/./discord.sh --text "I just restarted, make sure my containers are running"
-date >> restartdates.txt
+sudo docker run -d --restart always --name=www9 -p 8009:80 bookface:v1
+sudo docker run -d --restart always --name=www10 -p 8010:80 bookface:v1
+sudo docker run -d --restart always --name=www11 -p 8011:80 bookface:v1
+#/home/ubuntu/discord.sh-1.3/./discord.sh --text "I just restarted, make sure my containers are running"
+
