33a96e219b0e0d064f4080bd8ea56476868ab0ec
Ubuntu <asm492@github.com>
2020-03-06 11:23:59 +0000

    Skrevet om script

--- discord.sh-1.3/startserverv3.sh
+++ discord.sh-1.3/startserverv3.sh
@@ -6,66 +6,12 @@
 while true; do
 
 
-#cd ~
-#source IMT3003_V20_group25-openrc.sh
-#www1
-if curl -s --head --request GET 192.168.132.147 | grep "200 OK" > /dev/null; then
-:
-else
-cd ~
-source IMT3003_V20_group25-openrc.sh
-openstack server start www1 > /dev/null
-cd ~/discord.sh-1.3
-./discord.sh --text "@everyone WWW1 ISSA NO RESPOND, ISSA DON'T WORRY, I START AGIAN 4 U!" | date  >> siteuptime.txt;
-fi
-
-
-
-#cd ~
-#source IMT3003_V20_group25-openrc.sh
-#www2
-if curl -s --head --request GET 192.168.129.80  | grep "200 OK" > /dev/null; then
-:
-else
-cd ~
-source IMT3003_V20_group25-openrc.sh
-openstack server start www2 > /dev/null
-cd ~/discord.sh-1.3
-./discord.sh --text "@everyone WWW2 ISSA FAIL, ISSA I TRY TO START 4 U AGAIN" | date >> manageruptime.txt;
-fi
-
-#cd ~
-#source IMT3003_V20_group25-openrc.sh
-#balancer
-if curl -s --head --request GET 10.212.140.71  | grep "200 OK" > /dev/null; then
-:
-else
-cd ~
-source IMT3003_V20_group25-openrc.sh
-openstack server start Balancer > /dev/null
-cd ~/discord.sh-1.3
-./discord.sh --text "@everyone BALANCER issa old and ugly and dead, and his mom don't love him no more. ISSA i restart for you" | date >> manageruptime.txt;
-fi
-
-
-#cd ~
-#source IMT3003_V20_group25-openrc.sh
-#dockerserver
-if curl -s --head --request GET 192.168.131.240  | grep "200 OK" > /dev/null; then
-:
-else
-cd ~
-source IMT3003_V20_group25-openrc.sh
-openstack server start dockerserver > /dev/null
-cd ~/discord.sh-1.3
-./discord.sh --text "@everyone DOCKERSERVER issa no respond, issa i restart for u" | date >> manageruptime.txt;
-fi
-
-
 cd ~
 source IMT3003_V20_group25-openrc.sh
 openstack server start db1 > /dev/null
-
+openstack server start dockerden3. > /dev/null
+openstack server start Balancer > /dev/null
+openstack server start backup > /dev/null
 
 
 
--- scripts/managercontainerstart.sh
+++ scripts/managercontainerstart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Kjøres fra crontab -e: @reboot /home/ubuntu/containerstart.sh
 sudo docker stop $(sudo docker ps -a -q)
-sudo docker container rm www3 www4 www5 www6 www7 www8
+sudo docker container rm M1-C3 M1-C4 M1-C5 M1-C6 M1-C6 M1-C7 M1-C8
 
-sudo docker run -d --restart always --name=www3 -p 8003:80 bookface:v1
-sudo docker run -d --restart always --name=www4 -p 8004:80 bookface:v1
-sudo docker run -d --restart always --name=www5 -p 8005:80 bookface:v1
-sudo docker run -d --restart always --name=www6 -p 8006:80 bookface:v1
-sudo docker run -d --restart always --name=www7 -p 8007:80 bookface:v1
-sudo docker run -d --restart always --name=www8 -p 8008:80 bookface:v1
+sudo docker run -d --restart always --name=M1-C3 -p 8003:80 manager:5000/bf:latest
+sudo docker run -d --restart always --name=M1-C4 -p 8004:80 manager:5000/bf:latest
+sudo docker run -d --restart always --name=M1-C5 -p 8005:80 manager:5000/bf:latest
+sudo docker run -d --restart always --name=M1-C6 -p 8006:80 manager:5000/bf:latest
+sudo docker run -d --restart always --name=M1-C7 -p 8007:80 manager:5000/bf:latest
+sudo docker run -d --restart always --name=M1-C8 -p 8008:80 manager:5000/bf:latest
