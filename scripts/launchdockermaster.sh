1075b916b931b392632ea7142e4c266e19f788f5
asm492 <asm492@github.com>
2020-03-06 09:18:24 +0000

    orker ikke

--- scripts/launchdockermaster.sh
+++ scripts/launchdockermaster.sh
@@ -6,13 +6,13 @@ openssl req -newkey rsa:4096 -nodes -sha256 \
         -out registry_certs/domain.cert
 ls registry_certs/
 
-docker run -d -p 5000:5000 \
+sudo docker run -d -p 5000:5000 \
                 -v $(pwd)/registry_certs:/certs \
                 -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.cert \
                 -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
                 --restart=always --name registry registry:2
                 
-docker push localhost:5000/proxy:1.0.0
+sudo docker push localhost:5000/proxy:1.0.0
 
 
 sudo docker init
