f8f0f2b0c5419ff627b6775c3fa1bf7aae7c00f3
Ubuntu <asm492@github.com>
2020-03-06 14:03:49 +0000

    endret script for dockerworker

--- scripts/launchdockerserver.sh
+++ scripts/launchdockerserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#DETTE scriptet skal kun kjøres på docker workers!
 sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -8,3 +8,8 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo docker run hello-world
+sudo docker swarm join --token SWMTKN-1-27hr8uptn2kc5e24lha7syhu8feo37d03efvc3oaq3poeitd7q-azqyx03fye19o958jnk2kd2e4 192.168.130.196:2377
+mkdir -p /etc/docker/certs.d/manager:5000
+cp ~/thegoddiggers/scripts/registry_certs/domain.cert /etc/docker/certs.d/manager:5000/ca.crt
+sudo service docker restart
+sudo docker pull manager:5000/bf:latest
