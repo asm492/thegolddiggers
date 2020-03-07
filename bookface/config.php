87659287fa91d70a3c111a9538e39b31c28df699
asm492 <60274300+asm492@users.noreply.github.com>
2020-03-07 15:50:26 +0100

    Update config.php

--- bookface/config.php
+++ bookface/config.php
@@ -9,7 +9,8 @@ $weburl = 'http://' . $webhost ;
 $frontpage_limit = "500";
 
 #Remember to change this IP every time you make a new dockerserver
-$memcache_server = "192.168.128.138";
+#192.168.130.196 = master/manager
+$memcache_server = "192.168.130.196";
 $memcache_enabled = 1;
 $memcache_enabled_pictures = 1;
 
