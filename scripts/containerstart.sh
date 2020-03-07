b949546d777531f50ef01d6866ff0802f29063d9
asm492 <asm492@github.com>
2020-03-07 14:54:39 +0000

    lol

--- scripts/containerstart.sh
+++ scripts/containerstart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Kjøres fra crontab -e: @reboot /home/ubuntu/containerstart.sh
 sudo docker stop $(sudo docker ps -a -q)
-sudo docker container rm www11 www10 www9 memcache
+sudo docker container rm memcache
 
-sudo docker run -d --restart always --name=www9 -p 8009:80 bookface:v1
-sudo docker run -d --restart always --name=www10 -p 8010:80 bookface:v1
-sudo docker run -d --restart always --name=www11 -p 8011:80 bookface:v1
-sudo docker run -d --restart always --name=memcache -p 11211:11211 -d memcached memcached -m 256
+#sudo docker run -d --restart always --name=www9 -p 8009:80 bookface:v1
+#sudo docker run -d --restart always --name=www10 -p 8010:80 bookface:v1
+#sudo docker run -d --restart always --name=www11 -p 8011:80 bookface:v1
+#sudo docker run -d --restart always --name=memcache -p 11211:11211 -d memcached memcached -m 256
