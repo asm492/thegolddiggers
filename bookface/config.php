9e65872d263b9b3c73d32fe10f82ddfaf3375f12
asm492 <60274300+asm492@users.noreply.github.com>
2020-03-08 14:43:46 +0100

    Økt frontpage limit til 2000

--- bookface/config.php
+++ bookface/config.php
@@ -6,7 +6,7 @@ $dbuser = "bfuser";
 $dbpassw = '';
 $webhost = "10.212.140.71";
 $weburl = 'http://' . $webhost ;
-$frontpage_limit = "500";
+$frontpage_limit = "2000";
 
 #Remember to change this IP every time you make a new dockerserver
 #192.168.130.196 = master/manager
