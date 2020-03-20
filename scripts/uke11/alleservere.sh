5d33ce8c5bf10292b7171dfd4ae3a7a5584ebbbf
Ubuntu <asm492@github.com>
2020-03-20 08:47:01 +0000

    Lagt til verbose

--- scripts/uke11/alleservere.sh
+++ scripts/uke11/alleservere.sh
@@ -4,8 +4,8 @@
 sudo apt-get -y install glusterfs-server glusterfs-client
 sudo systemctl enable glusterd
 sudo systemctl start glusterd
-sudo mkdir /bf_brick
-sudo mkdir /bf_config_brick
+sudo mkdir /bf_brick -v
+sudo mkdir /bf_config_brick -v
 # Vi lager også to mapper som blir der vi "mounter" de distribuerte filsystemene. Det er disse mappene vi så skal bruke. 
-sudo mkdir /bf_images
-sudo mkdir /bf_config
+sudo mkdir /bf_images -v
+sudo mkdir /bf_config -v
