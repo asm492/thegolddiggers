5967efc030cbdafc8dc922ac927e86b1c7b7e6f6
asm492 <60274300+asm492@users.noreply.github.com>
2020-04-15 16:36:06 +0200

    Update cockroachDB.sh

--- scripts/cockroachDB.sh
+++ scripts/cockroachDB.sh
@@ -3,4 +3,4 @@ wget https://binaries.cockroachdb.com/cockroach-v19.2.2.linux-amd64.tgz
 tar xzf cockroach-v19.2.2.linux-amd64.tgz 
 sudo cp cockroach-v19.2.2.linux-amd64/cockroach /usr/local/bin 
 sudo mkdir /bfdata
-echo "sudo cockroach start --insecure --store=/bfdata --listen-addr=0.0.0.0:26257 --http-addr=0.0.0.0:8080 --background --join=192.168.130.196:26257,192.168.129.102:26257,192.168.128.138:26257 --advertise-addr=192.168.130.196:26257"
+sudo cockroach start --insecure --store=/bfdata --listen-addr=0.0.0.0:26257 --http-addr=0.0.0.0:8080 --background --join=192.168.130.196:26257,192.168.129.102:26257,192.168.128.138:26257 --advertise-addr=192.168.130.196:26257
