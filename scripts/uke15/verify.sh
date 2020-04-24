3fbe90d20fec7a6db42a18688964eb14e23f2c9d
Ubuntu <asm492@github.com>
2020-04-24 09:58:38 +0000

    Små endringer

--- scripts/uke15/verify.sh
+++ scripts/uke15/verify.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-#Sjekker om alt funker på manager
+
+printf "\n\n\n\n"
+
+echo "Sjekker om alt funker pa manager (Uke 15)"
+printf "\n\n"
 echo "==========sudo docker logs consul============"
 sudo docker logs consul
 
 echo "==========netstat -anltp====================="
-netstat -anltp
+netstat -anltp | grep 8500
 
 echo "==========sudo docker logs registrator======="
 sudo docker logs registrator
@@ -16,4 +20,5 @@ sudo docker logs consul_notifier
 echo "===================DIG======================="
 dig @192.168.128.5 bf.service..consul. ANY
 
+printf "\n\n"
 
